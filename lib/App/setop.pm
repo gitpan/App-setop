2133c184af8c534e261949861fec58b50f333c7c
Steven Haryanto <stevenharyanto@gmail.com>
2014-04-04 07:03:43 +0000

    Import of SHARYANTO/App-setop-0.01 from CPAN.
    gitpan-cpan-distribution: App-setop
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/App-setop-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/App/setop.pm
@@ -0,0 +1,53 @@
+package App::setop;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Set operations (union, intersection, difference) on lines of files
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::setop - Set operations (union, intersection, difference) on lines of files
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+See the command-line script L<setop>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-setop>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-App-setop>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-setop>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
